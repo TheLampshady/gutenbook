fb4c250bf4d6c40dfd0f103de21dcd95ee985df3
TheLampshady <lampshady24@gmail.com>
2015-05-02 17:30:57 -0400

    Program arguments added.

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+#!/usr/local/bin/python
+
 import argparse
 from ebook import GutenbergEBook
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='Process ebooks.')
-	parser.add_argument('integers', metavar='N', type=int, nargs='+',
-					   help='an integer for the accumulator')
-	parser.add_argument('--sum', dest='accumulate', action='store_const',
-					   const=sum, default=max,
-					   help='sum the integers (default: find the max)')
+	parser.add_argument('-b', '--book', 
+						type=int,  dest='book_id',
+						required=True, 
+					    help='Book Number for EBook')
+	parser.add_argument('-r', '--remote', 
+						dest='remote', action='store_true',
+					    help='Retrieves the ebook from gutenberg.org')
 
 	return parser.parse_args()
-	print(args.accumulate(args.integers))
 
 if __name__ == "__main__":
    args = parse_args()
+   print args.book_id
+   print args.remote
